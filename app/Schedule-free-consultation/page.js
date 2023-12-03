e40c235407c26431472dcef39e070506ec5459c3
pravesh.goyal <praveshgoyal26f97@gmail.com>
2023-12-03 17:42:09 +0530

    about us starting

---
+++ app/Schedule-free-consultation/page.js
@@ -0,0 +1,12 @@
+export default function consult() {
+  let name = "Pravesh Goyal";
+  return (
+    <div>
+      <div class="rounded-xl consult_wrapper mx-auto my-5">
+        <div>home</div>
+        <div>|</div>
+        <div>portfolio</div>
+      </div>
+    </div>
+  );
+}
